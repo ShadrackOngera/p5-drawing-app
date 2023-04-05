47615cc9daf7304d1e11f249c9486d96b9816e07
shadrackongera <shadrack.sme@gmail.com>
2023-04-05 18:42:37 +0300

    zoom code

--- sketch.js
+++ sketch.js
@@ -1,70 +1,90 @@
 // Global variables that will store the toolbox colour palette
 // and the helper functions.
-var toolbox = null;
-var colourP = null;
-var helpers = null;
-var c;
-var currentStep = -1;
+let toolbox;
+let colourP;
+let helpers;
+let canvasContainer;
+let c;
+let currentStep = -1;
 let history = [];
+let zoom = 1;
 
 function setup() {
+  // create a canvas to fill the content div from index.html
+  canvasContainer = select('#content');
+  c = createCanvas(canvasContainer.size().width, canvasContainer.size().height);
+  c.parent("content");
+  canvasContainer.elt.focus(); // set focus on the canvas element
 
-	//create a canvas to fill the content div from index.html
-	canvasContainer = select('#content');
-	c = createCanvas(canvasContainer.size().width, canvasContainer.size().height);
-	c.parent("content");
+  // create helper functions and the colour palette
+  helpers = new HelperFunctions();
+  colourP = new ColourPalette();
 
-	//create helper functions and the colour palette
-    helpers = new HelperFunctions();
-	colourP = new ColourPalette();
+  // create a toolbox for storing the tools
+  toolbox = new Toolbox();
 
-	//create a toolbox for storing the tools
-	toolbox = new Toolbox();
+  // add some tools to the toolbox
+  const mySprayCan = new SprayCanTool("My Spray Can", "assets/sprayCan.jpg", 10, 5);
+  toolbox.addTool(new FreehandTool());
+  toolbox.addTool(new LineToTool());
+  toolbox.addTool(mySprayCan);
+  toolbox.addTool(new mirrorDrawTool());
 
-	const mySprayCan = new SprayCanTool("My Spray Can", "assets/sprayCan.jpg", 10, 5);
+  // set the initial background color
+  background(255);
 
-	//add the tools to the toolbox.
-	toolbox.addTool(new FreehandTool());
-	toolbox.addTool(new LineToTool());
-	toolbox.addTool(mySprayCan);
-	toolbox.addTool(new mirrorDrawTool());
-	background(255);
-
-	// enable keyboard shortcuts for undo and redo
-	canvasContainer.elt.tabIndex = 0;
-	canvasContainer.elt.addEventListener('keydown', function(e) {
-		if (e.ctrlKey && e.key === 'z') { // Ctrl + Z
-			undo();
-		} else if (e.ctrlKey && e.key === 'y') { // Ctrl + Y
-			redo();
-		}
-	});
+  // enable keyboard shortcuts for undo and redo
+  canvasContainer.elt.tabIndex = 0;
+  canvasContainer.elt.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key === 'z') { // Ctrl + Z
+      undo();
+    } else if (e.ctrlKey && e.key === 'y') { // Ctrl + Y
+      redo();
+    }
+  });
 }
 
 function draw() {
-	if (toolbox.selectedTool.hasOwnProperty("draw")) {
-		toolbox.selectedTool.draw();
-		if (Array.isArray(history)) {
-			currentStep++;
-			history.splice(currentStep, history.length - currentStep, c.get());
-		}
-	} else {
-		alert("it doesn't look like your tool has a draw method!");
-	}
+  if (toolbox.selectedTool.hasOwnProperty("draw")) {
+    // if the selected tool has a draw method, call it
+    toolbox.selectedTool.draw();
+
+    // save the current canvas state to the history array
+    if (Array.isArray(history)) {
+      currentStep++;
+      history.splice(currentStep, history.length - currentStep, c.get());
+      scale(zoom); // apply zoom factor
+    }
+  } else {
+    alert("it doesn't look like your tool has a draw method!");
+  }
 }
 
 function undo() {
-	if (currentStep > 0) {
-		currentStep--;
-		c.background(255);
-		c.image(history[currentStep], 0, 0);
-	}
+  // move one step back in the history array and redraw the canvas
+  if (currentStep > 0) {
+    currentStep--;
+    c.background(255);
+    c.image(history[currentStep], 0, 0);
+  }
 }
 
 function redo() {
-	if (currentStep < history.length - 1) {
-		currentStep++;
-		c.background(255);
-		c.image(history[currentStep], 0, 0);
-	}
+  // move one step forward in the history array and redraw the canvas
+  if (currentStep < history.length - 1) {
+    currentStep++;
+    c.background(255);
+    c.image(history[currentStep], 0, 0);
+  }
+}
+
+function mouseWheel(event) {
+  // adjust the zoom based on the mouse wheel delta
+  zoom += event.delta * 0.001;
+  
+  // constrain the zoom to reasonable limits
+  zoom = constrain(zoom, 0.5, 5);
+
+  // prevent the page from scrolling
+  return false;
 }
